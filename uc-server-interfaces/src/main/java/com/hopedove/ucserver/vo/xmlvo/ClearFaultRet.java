c3b0c581978347fc98867944024ce2ccc8aa3462
BLW <874387643@qq.com>
2020-10-15 17:25:34 +0800

    调试同步接口

---
+++ uc-server-interfaces/src/main/java/com/hopedove/ucserver/vo/xmlvo/ClearFaultRet.java
@@ -0,0 +1,35 @@
+package com.hopedove.ucserver.vo.xmlvo;
+
+import javax.xml.bind.annotation.*;
+import java.util.List;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = {
+        "seqno",
+        "SubItem",
+        }) //控制JAXB 绑定类中属性和字段的排序
+@XmlRootElement(name="ClearFaultRet")
+public class ClearFaultRet {
+
+    @XmlAttribute
+    private String seqno;
+
+    @XmlElement
+    private List<SubItem> SubItem;
+
+    public String getSeqno() {
+        return seqno;
+    }
+
+    public void setSeqno(String seqno) {
+        this.seqno = seqno;
+    }
+
+    public List<com.hopedove.ucserver.vo.xmlvo.SubItem> getSubItem() {
+        return SubItem;
+    }
+
+    public void setSubItem(List<com.hopedove.ucserver.vo.xmlvo.SubItem> subItem) {
+        SubItem = subItem;
+    }
+}
